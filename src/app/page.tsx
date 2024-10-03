9b8a433ef5d329265f6db70a172f9e2bfd9e7111
marsafyyy <mokhallid72@gmail.com>
2024-10-03 07:08:42 +0300

    footbar component ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 // Page imports
 import Movies from '@/app/content/movies/page';
 
+// Components imports
+import Footbar from '@/app/components/appbars/Footbar';
+
 export default function Home() {
   return (
     <div className="home">
